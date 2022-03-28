60c1187a1166528412ac60a7bee1467f96dd9be6
Kai Parry <tp@threadproc.io>
2022-03-28 14:17:22 -0400

    change readme to use setup script, try to automatically install zsh

--- README.md
+++ README.md
@@ -6,29 +6,8 @@
 Install the patched font - it's pretty good and has all the icons:
 https://github.com/romkatv/powerlevel10k#meslo-nerd-font-patched-for-powerlevel10k
 
-### Empty home directory
 ```
-git clone --separate-git-dir=~/.dotfiles git@github.com:threadproc/dotfiles.git ~
-alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-dotfiles config status.showUntrackedFiles no
-dotfiles submodule init
-dotfiles submodule update
+curl -s -L dotfiles.threadproc.io | sh
 ```
-Then restart zsh
 
-### Non-empty home directory
-
-**NOTE**: this is untested still, but you get the idea. Clone into a temp dir, then pull again
-with a different work tree.
-
-```
-git clone --separate-git-dir=$HOME/.dotfiles git@github.com:threadproc/dotfiles.git $HOME/dotfiles-tmp
-cp ~/dotfiles-tmp/.gitmodules ~
-rm -r ~/dotfiles-tmp/
-alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-dotfiles config status.showUntrackedFiles no
-dotfiles reset --hard
-dotfiles submodule init
-dotfiles submodule update
-```
 Then restart zsh
--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -5,6 +5,7 @@ set +o errexit
 DOTFILES_DIR="$HOME/.dotfiles"
 DOTFILES_REPO="git@github.com:threadproc/dotfiles.git"
 DOTFILES_TMP="$HOME/df-tmp"
+OS=$(uname)
 
 GITPATH=$(which git)
 
@@ -15,6 +16,27 @@ else
     echo "Installing dotfiles..."
     rm -rf "$DOTFILES_TMP" # cleanup after ourselves if this script previously failed
     
+    # check for zsh
+    if [ ! `command -v zsh &>/dev/null` ]; then
+        if [ "$OS" == "Darwin" ]; then
+            echo "Installing zsh with homebrew..."
+            brew install zsh
+        elif [ "$OS" == "Linux" ]; then
+
+            if [ ! `command -v apt-get &>/dev/null` ]; then
+                echo "Running on a non-Debian system, you must install zsh manually!"
+                exit 1
+            fi
+
+            apt-get install zsh
+
+        else
+            echo "You must install zsh to use these dotfiles!"
+            exit 1
+        fi
+        
+    fi
+
     # we want to get the submodules setup right away
     $GITPATH clone --separate-git-dir="$DOTFILES_DIR" "$DOTFILES_REPO" "$DOTFILES_TMP"
     cp "$DOTFILES_TMP/.gitmodules" "$HOME/.gitmodules"
